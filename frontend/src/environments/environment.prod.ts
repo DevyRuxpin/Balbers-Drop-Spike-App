1a32eeda8412c107c13fc84f302591fbe947e5d4
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-04-07 22:09:21 -0400

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: '/api'
-  };
-  
+  production: true,
+  apiUrl: process.env['API_BASE_URL'] || 'https://your-backend-service.onrender.com'
+};
