35d89c3e8dbd2bbf3e80e32ab2e8cc2942129272
DevyRuxpin <160616247+DevyRuxpin@users.noreply.github.com>
2025-04-07 22:28:54 -0400

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,9 +7,13 @@ from config import Config
 db = SQLAlchemy()
 jwt = JWTManager()
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(Config)
+    
+    # Render-specific DB fix
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+        app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace("postgres://", "postgresql://", 1)
     
     db.init_app(app)
     jwt.init_app(app)
